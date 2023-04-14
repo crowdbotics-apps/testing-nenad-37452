f0d9db0ab045017892737992768ba62bdbb8925f
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-14 07:52:55 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
-  name: "testing_nenad_37452",
-  url: "https://testing_nenad_37452.botics.co",
-  api: "https://testing_nenad_37452.botics.co/api/v1"
-}
+  "name": "testing_nenad_37452",
+  "url": "https://testing_nenad_37452.botics.co",
+  "api": "https://testing_nenad_37452.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = () => {// handle sign in logic
+  };
+
+  return <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <Text style={styles.title}>Sign In</Text>
+      <TextInput placeholder="Email" style={styles.input} value={email} onChangeText={setEmail} />
+      <TextInput placeholder="Password" style={styles.input} value={password} onChangeText={setPassword} secureTextEntry />
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.forgotPassword}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.signUp}>Don't have an account? Sign Up</Text>
+      </TouchableOpacity>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 20
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#007bff',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  forgotPassword: {
+    color: '#007bff',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    marginBottom: 20
+  },
+  signUp: {
+    color: '#007bff',
+    fontSize: 16,
+    textDecorationLine: 'underline'
+  }
+});
+export default SignInScreen;
