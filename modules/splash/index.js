5736a00b920fd89876621d4a76c931319bd25942
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-17 08:27:31 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/splash/index.js
+++ modules/splash/index.js
@@ -3,14 +3,19 @@ import { View } from "react-native";
 import PropTypes from "prop-types";
 import { OptionsContext } from "@options";
 
-const Splash = ({ duration, onDurationEnd }) => {
+const Splash = ({
+  duration,
+  onDurationEnd
+}) => {
   const options = useContext(OptionsContext);
 
   const handleDurationEnd = () => {
     options.hide();
+
     if (onDurationEnd) {
       onDurationEnd();
     }
+
     if (options.onDurationEnd) {
       options.onDurationEnd();
     }
@@ -25,18 +30,14 @@ const Splash = ({ duration, onDurationEnd }) => {
       handleDurationEnd();
     }
   }, []);
-
-  return (
-    <View>
-    </View>
-  );
+  return <View>
+    </View>;
 };
 
 Splash.propTypes = {
   duration: PropTypes.number,
   onDurationEnd: PropTypes.func
 };
-
 export default {
   title: "Splash",
   navigator: Splash
